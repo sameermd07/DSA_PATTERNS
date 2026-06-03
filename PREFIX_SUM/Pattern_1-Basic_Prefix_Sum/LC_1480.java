29c2f19b9a0752ac9ac95de0a2d8987eb2715586
sameermd07 <sameermd1857978@gmail.com>
2026-06-03 13:57:47 +0530

    first commit - first problem !!

---
+++ PREFIX_SUM/Pattern_1-Basic_Prefix_Sum/LC_1480.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+     int arr[]=new int[nums.length];
+     int j=0;
+     int sum=0;
+     for(int i:nums){
+        sum+=i;
+        arr[j]=sum;
+        j++;
+     }   
+     return arr;
+    }
+}
